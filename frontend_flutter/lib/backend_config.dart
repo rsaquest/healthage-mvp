4e629ff201b68a69d1913fccc192a1deb814cebe
samsudeenrajih-glitch <samsudeen.rajih@jumia.com>
2026-09-18 12:48:17 +0100

    Publish Flutter web app

--- frontend_flutter/lib/backend_config.dart
+++ frontend_flutter/lib/backend_config.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/foundation.dart';
 
 class BackendConfig {
-  // For web, use localhost. For Android/iOS devices, use localhost with adb reverse
-  // set up by running: adb reverse tcp:3000 tcp:3000
-  static String get httpBase => kIsWeb ? 'http://localhost:3000' : 'http://127.0.0.1:3000';
-  static String get wsUrl => kIsWeb ? 'ws://localhost:3000/ws' : 'ws://127.0.0.1:3000/ws';
+  static const String productionHttpBase = 'https://healthage-backend.onrender.com';
+  static const String productionWsUrl = 'wss://healthage-backend.onrender.com/ws';
+
+  static String get httpBase => kReleaseMode
+      ? productionHttpBase
+      : (kIsWeb ? 'http://localhost:3000' : 'http://127.0.0.1:3000');
+
+  static String get wsUrl => kReleaseMode
+      ? productionWsUrl
+      : (kIsWeb ? 'ws://localhost:3000/ws' : 'ws://127.0.0.1:3000/ws');
 }
